e250068982657635b7145bc62d50f124911cc239
CORP\lalac <achintya.lal@powerschool.com>
2019-12-16 15:27:26 +0530

    My second commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public class A
+    {
+        public virtual void print()
+        {
+            Console.Write("A");
+        }
+    }
+    public class B : A
+    {
+        public override void print()
+        {
+            Console.Write("B");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            A obj = new A();
+            obj.print();
+            Console.ReadKey();
         }
     }
 }
